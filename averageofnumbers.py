bcb58e3b992416a28e7d545a8ad6b30947a9feea
Alev Nyaa <alevnyaa@gmail.com>
2017-01-07 02:47:11 +0300

    Reorganized repository for exercises in multiple languages

--- averageofnumbers.py
+++
@@ -1,8 +0,0 @@
-s = input()
-numbers = list(map(int, s.split()))
-
-sumofnum = sum(number for number in numbers)
-
-avr = sumofnum / len(numbers)
-
-print(avr)
--- sumofmultiplesof3and5.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python
-print(sum(n for n in range(1000) if not (n%3 and n%5)))
