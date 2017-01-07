9431532743c05971b4e632c06d68efccf69fbb62
Alev Nyaa <alevnyaa@gmail.com>
2017-01-07 04:38:29 +0300

    Deciding on best approach to exercise 2. The low level nature of C++ is causing problems.

--- cpp/bau-cmp1001-final-practice/2-arrays.cpp
+++ cpp/bau-cmp1001-final-practice/2-arrays.cpp
@@ -1,2 +1,29 @@
 //get char array of size 10 and a specific char
 //function to return number of times a char is in the array 
+
+#include <iostream>
+using namespace std;
+/*
+int howMany(char[] word, char ch){
+  int count = 0;
+  for(int i=0; i<10; i++){
+    if(word[i] == ch){
+      count++;
+    }
+  }
+  return count;
+}
+*/
+int main(){
+  char word[10];
+  for(int i=0; i<10; i++){
+    char ch;
+    cin >> ch;
+    word[i] = ch;
+  }
+  cout << endl;
+  for(int i=0; i<10; i++){
+    cout >> word[i];
+  }
+  //cout << howMany(word, 'a');
+}
