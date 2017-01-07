3d4d531f2c3958f3ed732c3e7c2aced731884f52
Alev Nyaa <alevnyaa@gmail.com>
2017-01-07 04:21:09 +0300

    Ignored .out files and finished first exercise

--- cpp/bau-cmp1001-final-practice/1-arrays.cpp
+++ cpp/bau-cmp1001-final-practice/1-arrays.cpp
@@ -1,2 +1,21 @@
 //get 10 doubles into array
 //reverse the order
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  double num[10];
+  cout << "Input:" << endl;
+  for(int i=0; i<10; i++){
+    cin >> num[i];
+  }
+  double reversed[10];
+  for(int i=0; i<10; i++){
+    reversed[i] = num[9-i];
+  }
+  cout << "Reversed:" << endl;
+  for(int i=0; i<10; i++){
+    cout << reversed[i] << endl;
+  }
+}
