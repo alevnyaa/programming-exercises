55cb21de8b1100cd366a4fa15f0d2f9e290a9dcc
Alev Nyaa <alevnyaa@gmail.com>
2017-01-16 02:28:48 +0300

    Finished 10th exercise, drawing triangles.

--- cpp/bau-cmp1001-final-practice/10-drawingwithforloops.cpp
+++ cpp/bau-cmp1001-final-practice/10-drawingwithforloops.cpp
@@ -1,2 +1,61 @@
 //draw dynamic size (get from user) triangles
 //with all 4 possible shapes
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Input size of triangles." << endl;
+  int num;
+  cin >> num;
+  int curline = 0;
+  
+  for(int i=0; i<num; i++){
+    curline++;
+    for(int j=0; j<curline; j++){
+      cout << "*";  
+    } 
+    cout << endl;
+  }
+
+  cout << "--------------------" << endl;
+
+  curline = num + 1;
+  for(int i=0; i<num; i++){
+    curline--;
+    for(int j=0; j<curline; j++){
+      cout << "*";  
+    } 
+    cout << endl;
+  }
+
+  cout << "--------------------" << endl;
+  
+  int spaces = 0;
+
+  for(int i=0; i<num; i++){
+    for(int j=0; j<spaces; j++){
+      cout << " ";
+    }
+    for(int j=0; j<num-spaces; j++){
+      cout << "*";
+    }
+    spaces++;
+    cout << endl;
+  }
+
+  cout << "--------------------" << endl;
+ 
+  spaces = num-1;
+
+  for(int i=0; i<num; i++){
+    for(int j=0; j<spaces; j++){
+      cout << " ";
+    }
+    for(int j=0; j<num-spaces; j++){
+      cout << "*";
+    }
+    spaces--;
+    cout << endl;
+  }
+}
