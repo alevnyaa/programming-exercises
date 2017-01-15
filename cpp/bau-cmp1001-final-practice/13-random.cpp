c5865175193554644aafb0fa29772a3c0850a5fb
Alev Nyaa <alevnyaa@gmail.com>
2017-01-16 02:38:13 +0300

    13th exercise, random, complete.

--- cpp/bau-cmp1001-final-practice/13-random.cpp
+++ cpp/bau-cmp1001-final-practice/13-random.cpp
@@ -3,3 +3,28 @@
 //make sure to seed random
 //calc the sum of even numbers in odd indexes
 //such as 1st, 3rd, 5th index
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main(){
+  srand(time(NULL));
+
+  int arr[50];
+
+  for(int i=0; i<50; i++){
+    arr[i] = rand() % 250 + 1;
+  }
+  
+  int sum = 0;
+
+  for(int i=1; i<50; i+=2){
+    if(arr[i]%2==0){
+      sum += arr[i];
+    }
+  }
+
+  cout << "The sum of even numbers in odd indexes is " << sum << "." << endl;
+}
