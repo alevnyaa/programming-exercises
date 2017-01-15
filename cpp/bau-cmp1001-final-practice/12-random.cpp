b1aa13ab794cd0c4fc04497f6399543de1c62e70
Alev Nyaa <alevnyaa@gmail.com>
2017-01-16 02:34:49 +0300

    12th exercise, rolling two dices and counting doubles, is complete.

--- cpp/bau-cmp1001-final-practice/12-random.cpp
+++ cpp/bau-cmp1001-final-practice/12-random.cpp
@@ -1,2 +1,31 @@
-//roll a dice 100 times
+//roll two dices 100 times
 //and count how many doubles you get
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main(){
+  int dice1[100];
+  int dice2[100];
+  
+  srand(time(NULL));
+  for(int i=0; i<100; i++){
+    dice1[i] = rand() % 6;
+  }
+  
+  for(int i=0; i<100; i++){
+    dice2[i] = rand() % 6;
+  }
+
+  int doubles = 0;
+
+  for(int i=0; i<100; i++){
+    if(dice1[i] == dice2[i]){
+      doubles++;
+    }
+  }
+
+  cout << "There were " << doubles << " doubles." << endl;
+}
