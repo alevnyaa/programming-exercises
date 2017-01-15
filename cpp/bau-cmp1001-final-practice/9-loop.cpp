5d6a58e919f9f7899c16970cc70309e3f25df2e1
Alev Nyaa <alevnyaa@gmail.com>
2017-01-16 01:57:00 +0300

    9th exercise complete.

--- cpp/bau-cmp1001-final-practice/9-loop.cpp
+++ cpp/bau-cmp1001-final-practice/9-loop.cpp
@@ -2,3 +2,21 @@
 //if odd, assing 3x+1
 //if even, x/2
 //iterate until you get 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Input an integer to be iterated over." << endl;
+  int num;
+  cin >> num;
+
+  while(num!=1){
+    if(num%2==0){
+      num/=2;
+    }else{
+      num = 3 * num + 1;
+    }
+    cout << num << endl;
+  }
+}
