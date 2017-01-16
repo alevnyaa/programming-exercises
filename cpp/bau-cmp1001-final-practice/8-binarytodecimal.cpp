3b60347257cadca2c5c8cf5e8972509caad91cd9
Alev Nyaa <alevnyaa@gmail.com>
2017-01-16 03:34:14 +0300

    8th challenge started.

--- cpp/bau-cmp1001-final-practice/8-binarytodecimal.cpp
+++ cpp/bau-cmp1001-final-practice/8-binarytodecimal.cpp
@@ -1,2 +1,31 @@
 //allowed to use mathematical libraries
 //get a binary num, checking for validity, and then display decimal value
+
+#include <iostream>
+using namespace std;
+
+int bin2dec(int n){
+  int output = 0;
+  for(int i=0; n > 0; i++) {
+    if(n % 10 == 1) {
+      output += (1 << i);
+    }
+    n /= 10;
+  }
+  return output;
+}
+
+bool checkBinary(int n){
+	return true;
+}
+
+int main(){
+	cout << "Input a binary number." << endl;
+	int binum;
+	cin >> binum;
+  if(!checkBinary(binum)){
+    return 1;
+  }
+  int decnum = bin2dec(binum);
+  cout << decnum << endl;
+}
