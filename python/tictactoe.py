776a659ff9cf86a63241ac655a53513843b8beb4
Katherine Door <alevnyaa@gmail.com>
2017-07-26 02:00:54 +0300

    Rewritten the tictactoe, missing display logic and victory checks. No plans for AI yet.

--- python/tictactoe.py
+++ python/tictactoe.py
@@ -1,26 +1,55 @@
 #!/usr/bin/env python
 
-def victory():
-    if(false):
-        finalized = True;
-        if(victoryConditionFor(0)):
-            print("First player has won!")
-        elif(victoryConditionFor(1)):
-            print("Second player has won!")
+current_player = 0
 
-def victoryConditionFor(player):
-    if(player==0):
-        player = "X"
-    elif(player==1):
-        player = "O"
+players = [{
+    'name' : 'X',
+    'victories' : 0
+    },{
+    'name' : 'O',
+    'victories' : 0
+    }]
 
-    for i in range(0,3):
-        for j in range(0,3):
+rounds_max = 9
+rounds_left = rounds_max
 
+print("Tic Tac Toe!\n")
 
-grid= [["X","X","O"],
-       ["O"," ","X"],
-       ["X"," ","O"]]
-finalized = False
-while not(victoryCheck):
-    display(grid)
+print("Continue with default player names?")
+
+if input("[y/n] ").lower() != 'y':
+    players[0]['name'] = input("Player one's name: ")
+    players[1]['name'] = input("Player two's name: ")
+
+board =  [[" " for x in range(3)] for y in range(3)]
+
+while rounds_left > 0:
+    # decrement round counter
+    rounds_left = rounds_left - 1
+
+    # print current player text
+    print("{}'s turn:".format(players[current_player]['name']))
+
+    # get valid column and row
+    valid_input = False;
+    while not valid_input:
+        column = int(input("Column: ")) - 1
+        row = int(input("Row: ")) - 1
+
+        if(column < 0
+                or row < 0
+                or column > 2
+                or row > 2):
+            print("This is not a cell on the board! Try again.")
+        else:
+            valid_input = True
+
+    # display board
+
+    # check if current player victory
+
+    # change current player
+    if current_player == 0:
+        current_player = 1
+    else:
+        current_player = 0
